cb295d96b687f4c1e3c18ffa8732b9ef142ca407
shagun choudhary <shagunchoudhary@virtualempl.com>
2024-01-08 12:10:59 +0530

    updated status code

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from .serializers import ProductReviewSerializer
 import openai
 from django.conf import settings
 from django.views.generic import TemplateView
+import json
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -15,39 +16,39 @@ class GetChatGPTSuggestions(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = ProductReviewSerializer(data=request.data)
-        if serializer.is_valid():
+        try:
+            serializer = ProductReviewSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
             star_rating = serializer.validated_data['star_rating']
             product_name = serializer.validated_data['product_name']
-            suggestions = self.get_chatgpt_suggestions(star_rating,product_name)                                                                                                                                                                                                                                                                                                                                                                        
 
-            if 'error' in suggestions:
-                return Response({'error': suggestions}, status=status.HTTP_400_BAD_REQUEST)
+            suggestions = self.get_chatgpt_suggestions(star_rating, product_name)
+            return Response({'suggestions': suggestions}, status=status.HTTP_200_OK)
 
-            return Response({'suggestions': suggestions})
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
 
     def get_chatgpt_suggestions(self, star_rating,product_name):
-        try:
-            openai.api_key = settings.OPEN_API_KEY
-            prompt = f"User rated the product{product_name} {star_rating}stars. Generate 5-8 best describing words for {star_rating} of {product_name}.Ignore description."
+        openai.api_key = settings.OPEN_API_KEY
+        prompt = f"User rated the product:{product_name} with {star_rating} stars. Generate 8-10 best describing words for {star_rating} of {product_name}, Ignore description and should be in the array format."
 
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant for the product review system."},
-                    {"role": "user", "content": prompt}
-                ],
-                max_tokens=60
-            )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant for the product review system."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=60
+        )
 
-            suggestions = response['choices'][0]['message']['content']
-            suggestions_list = [word.split(". ")[1] for word in suggestions.split("\n") if word.strip()]
-
-            # suggestions = "Neutral, average, satisfactory, acceptable, standard, decent, usual"
-            return  suggestions_list 
-        except Exception as e:
-            return str(e)
+        suggestions = response['choices'][0]['message']['content']
+        suggestions_list = json.loads(suggestions)
+         # suggestions = "Neutral, average, satisfactory, acceptable, standard, decent, usual"  
+        return suggestions_list
+       
+       
 
 
 class GetChatGPTReview(APIView):
@@ -55,34 +56,35 @@ class GetChatGPTReview(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = ProductReviewSerializer(data=request.data)
-        if serializer.is_valid():
+        try:
+            serializer = ProductReviewSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            
             star_rating = serializer.validated_data['star_rating']
             user_word = serializer.validated_data['user_word']
             product_name = serializer.validated_data['product_name']
-            review = self.get_chatgpt_review(star_rating, user_word,product_name)
+            
+            review = self.get_chatgpt_review(star_rating, user_word, product_name)
+            return Response({'review': review}, status=status.HTTP_200_OK)
 
-            if 'error' in review:
-                return Response({'error': review}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({'review': review})
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     def get_chatgpt_review(self, star_rating, user_word, product_name):
-        try:
-            openai.api_key = settings.OPEN_API_KEY
-            prompt = f"User selected {user_word} as best describing words for {product_name} and rated {star_rating} stars for review.Only provide a detailed 80-100 words review based on {product_name} in easy language. Ignore description."
+        openai.api_key = settings.OPEN_API_KEY
+        prompt = f"User selected {user_word} as best describing words for {product_name} and rated {star_rating} stars for review.Only provide a detailed 80-100 words review based on {product_name} in easy language. Ignore description."
 
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant for a product review system."},
-                    {"role": "user", "content": prompt}
-                ],
-                max_tokens=200
-            )
-            review = response['choices'][0]['message']['content']
-            # review = "The product was average in terms of performance. It did the job adequately. It was satisfactory and decent overall. The quality and durability were moderate, and it seemed to hold up fine under regular use. The features and functionality were acceptable, but there was nothing that stood out. I would give it 3 stars because it met my basic needs, but I wasn't overly impressed. If you're looking for a basic, no-frills product, this would be a decent choice."
-            return review
-        except Exception as e:
-            return str(e)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant for a product review system."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=200
+        )
+        review = response['choices'][0]['message']['content']
+        # review = "The product was average in terms of performance. It did the job adequately. It was satisfactory and decent overall. The quality and durability were moderate, and it seemed to hold up fine under regular use. The features and functionality were acceptable, but there was nothing that stood out. I would give it 3 stars because it met my basic needs, but I wasn't overly impressed. If you're looking for a basic, no-frills product, this would be a decent choice."
+        return review
+      
